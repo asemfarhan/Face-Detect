97ca468582ce296507c81676c9cf22cc08a9dbd0
asemfarhan <asem.mohammed.f@gmail.com>
2019-11-26 01:13:26 +0300

    Updates

---
+++ precache-manifest.68c17dc305052958178b24cb2399cae7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ecacdab0de2765e9c9eaea55ab3da44",
+    "url": "/asemfarhan/Face-Detect.git/index.html"
+  },
+  {
+    "revision": "df7d8ffa167d9e2a3f5d",
+    "url": "/asemfarhan/Face-Detect.git/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "e37b3bbc8ed0cf0d1bc4",
+    "url": "/asemfarhan/Face-Detect.git/static/css/main.5e296739.chunk.css"
+  },
+  {
+    "revision": "df7d8ffa167d9e2a3f5d",
+    "url": "/asemfarhan/Face-Detect.git/static/js/2.d0e019f7.chunk.js"
+  },
+  {
+    "revision": "e37b3bbc8ed0cf0d1bc4",
+    "url": "/asemfarhan/Face-Detect.git/static/js/main.2ef81e75.chunk.js"
+  },
+  {
+    "revision": "865883aff232ef9a4938",
+    "url": "/asemfarhan/Face-Detect.git/static/js/runtime-main.24c462e0.js"
+  },
+  {
+    "revision": "da203b4631c1da25e8f1e8a80613e584",
+    "url": "/asemfarhan/Face-Detect.git/static/media/logo.da203b46.png"
+  }
+]);
