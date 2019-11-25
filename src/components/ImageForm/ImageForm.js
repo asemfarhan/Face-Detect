ddf6aeca4dea2e19025adbd3f6cbda2fb6c3f486
asemfarhan <asem.mohammed.f@gmail.com>
2019-11-26 01:54:59 +0300

    Create a build directory in production

--- src/components/ImageForm/ImageForm.js
+++ src/components/ImageForm/ImageForm.js
@@ -4,16 +4,16 @@ const ImageForm = ( {onClickDetect_ImageForm , onChangeInputText_ImageForm} ) =>
     return(
         <div>
             <div  className='db center'>
-                <p className=' b pv2 tracked tc white '> 
+                <div><p className=' b pv2 tracked tc white '> 
                     {'Hellow to face detect website.'}
-                </p>
+                </p></div>
                 <div className='flex center '>
                     <input placeholder='http:\\...'
                         className='input-reset br2 mv1 b--black-20 pa2 mb2 db w-50 tc' 
                         onChange={onChangeInputText_ImageForm} 
                         type='text' />  
                     <button                    
-                        className='ph3 pv2 grow bg-black link bg-transparent hover-bg-black hover-white  w-20' 
+                        className='ph3 pv2 grow link white bg-black-50 hover-bg-black hover-white  w-20' 
                         onClick={onClickDetect_ImageForm} >Detect Face</button>
                 </div>
             </div>
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -5,7 +5,7 @@ import logo_img from '../../images/logo.png';
 const Logo = () =>{
     return(
         <div>
-            <Tilt className="Tilt br2 mh5 ma4 pa3 shadow-2 tc" options={{ max : 25 }} style={{ height: 180 , width: 180 }} >
+            <Tilt className="Tilt br2 mh5 ma4 pa3 bg-black-50 shadow-2 tc" options={{ max : 25 }} style={{ height: 160 , width: 160 }} >
                  <div className="Tilt-inner"><img className="" alt="Logo" src={logo_img}/> </div>
             </Tilt>
 
